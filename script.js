c35541622a5863761728bd5950ad87d2d91c665b
kov-edit <kovacs.edit@diak.szbi-pg.hu>
2024-11-07 12:54:10 +0100

    1253

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-//menu
+//menu-főoldal
 function openNav() {
   document.getElementById("mySidenav").style.width = "400px";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+}
+
+//menu - mindenhol máshol
+function openNav1() {
+  document.getElementById("mySidenav1").style.width = "250px";
+  document.getElementById("main1").style.marginLeft = "250px";
+}
+
+function closeNav1() {
+  document.getElementById("mySidenav1").style.width = "0";
+  document.getElementById("main1").style.marginLeft= "0";
 }
